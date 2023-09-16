7b3b17a2756c17e535c55e0a80840364e19b943c
Sumit Modak <142516692+real-sm@users.noreply.github.com>
2023-09-16 14:27:48 +0530

    added order model & placing order system

--- shop/models.py
+++ shop/models.py
@@ -21,4 +21,19 @@ class Contact(models.Model):
 
     def __str__(self):
         return self.name
-    
+
+class Order(models.Model):
+    order_id = models.AutoField(primary_key=True)
+    items_json = models.CharField(max_length=512)
+    name = models.CharField(max_length=32)
+    email = models.CharField(max_length=32)
+    phone = models.IntegerField(default=0)
+    address = models.CharField(max_length=64)
+    landmark = models.CharField(max_length=64)
+    pincode = models.IntegerField(default=0)
+    town_city = models.CharField(max_length=32)
+    state = models.CharField(max_length=32)
+    country = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
--- shop/views.py
+++ shop/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-from .models import Product, Contact
+from .models import Product, Contact, Order
 from math import ceil
 
 def index(request):
@@ -41,10 +41,28 @@ def contact(request):
     return render(request, "shop/contact.html")
 
 def checkout(request):
+    if request.method == "POST":
+        itemsJSON = request.POST.get("itemsJSON", "")
+        name = request.POST.get("inputName", "")
+        email = request.POST.get("inputEmail", "")
+        phone = request.POST.get("inputPhone", "")
+        address = request.POST.get("inputAddress", "")
+        landmark = request.POST.get("inputLandmark", "")
+        pincode = request.POST.get("inputPinCode", "")
+        town_city = request.POST.get("inputTownCity", "")
+        state = request.POST.get("inputState", "")
+        country = request.POST.get("inputCountry", "")
+
+        order = Order(items_json=itemsJSON, name=name, email=email, phone=phone,
+                      address=address, landmark=landmark, pincode=pincode,
+                      town_city=town_city, state=state, country=country)
+        order.save()
+        return render(request, "shop/checkout.html", {"thank": True, "id": order.order_id})
+        
     return render(request, "shop/checkout.html")
 
 def tracker(request):
-    return render(request, ".html")
+    return render(request, "shop/tracker.html")
 
 def search(request):
     return render(request, ".html")
