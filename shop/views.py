e43905c5444675dc9b3ab15cfdaa8e1875fa6cf3
Sumit Modak <142516692+real-sm@users.noreply.github.com>
2023-09-16 09:38:49 +0530

    changed /shop/checkout ui

--- shop/views.py
+++ shop/views.py
@@ -40,11 +40,11 @@ def contact(request):
 
     return render(request, "shop/contact.html")
 
+def checkout(request):
+    return render(request, "shop/checkout.html")
+
 def tracker(request):
     return render(request, ".html")
 
 def search(request):
-    return render(request, ".html")
-
-def checkout(request):
     return render(request, ".html")
