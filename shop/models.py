0fe4906d920e1e674cdf405474672257065b5feb
Sumit Modak <142516692+sumit-modak@users.noreply.github.com>
2024-08-02 14:13:23 +0530

    changed shop/models and templates/shop/tracker

--- shop/models.py
+++ shop/models.py
@@ -37,3 +37,12 @@ class Order(models.Model):
 
     def __str__(self):
         return self.name
+
+class OrderUpdate(models.Model):
+    update_id = models.AutoField(primary_key=True)
+    order_id = models.IntegerField(default="")
+    update_desc = models.CharField(max_length=1024)
+    timestamp = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return self.update_desc[0:15] + "..."
